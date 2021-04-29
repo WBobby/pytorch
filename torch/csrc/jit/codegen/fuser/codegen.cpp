bca2dc294714b2d276bd4cc7c8c8474bdb4ad3c6
Your Name <you@example.com>
2021-04-29 22:01:16 +0000

    must unconditionally define RuntimeHeader codegen key

--- torch/csrc/jit/codegen/fuser/codegen.cpp
+++ torch/csrc/jit/codegen/fuser/codegen.cpp
@@ -675,6 +675,9 @@ std::string generateKernel(
 #include <hip/hip_runtime.h>
 )");
   }
+#else
+  // Still need the key defined, but empty.
+  env.s("RuntimeHeader", R"()");
 #endif
 #endif
   // clang-format on
